c0c0f05872991a1fd1c632deaf6041864a365997
dajun928 <1663177102@qq.com>
2018-09-19 23:08:25 +0800

    update
    update

--- PrintDemo/helloworld.py
+++ PrintDemo/helloworld.py
@@ -2,7 +2,6 @@
 #-*- coding:utf-8 -*-
 '''
     工具脚本 201809月份日记格式
-
 '''
 def get_week_day(i):
   week_day_dict = {
@@ -21,7 +20,6 @@ for i in range(1,31):
     print '-------------------------------------------------------------->'
     print u"2018年9月"+str(i)+u"日"+"  "+get_week_day(d)
     print '  '
-
 
 
 
--- Tools/Download.py
+++ Tools/Download.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
 import requests
 from bs4 import BeautifulSoup
 import os
