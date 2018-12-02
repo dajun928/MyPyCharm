98a537a38593eaf1d7571740fd9f495d07a8241a
dajun928 <1663177102@qq.com>
2018-12-02 10:52:24 +0800

    update

--- PrintDemo/helloworld.py
+++ PrintDemo/helloworld.py
@@ -16,9 +16,9 @@ def get_week_day(i):
   return week_day_dict[i]
 
 for i in range(1,31):
-    d=(i+4)%7
+    d=(i+2)%7
     print '-------------------------------------------------------------->'
-    print u"2018年9月"+str(i)+u"日"+"  "+get_week_day(d)
+    print u"2018年11月"+str(i)+u"日"+"  "+get_week_day(d)
     print '  '
 
 
